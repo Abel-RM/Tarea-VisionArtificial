aacb5d4cb42c9dbf90e2a582e5680dbb9f403b5e
Abel Robles <abelyrobles@gmail.com>
2021-02-21 16:33:50 -0700

    terminado

---
+++ main.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy
+
+def add_ceros(imagerArr):
+
+    for fila in imagerArr:
+        fila.insert(0, 160)
+        fila.append(160)
+
+    ls = [160] * len(imagerArr[0])
+
+    imagerArr.insert(0, ls)
+    imagerArr.append(ls)
+
+    return imagerArr
+
+
+def binary_to_decimal(lista):
+    result = 0
+    i = 0
+    for bit in reversed(lista):
+        result += (2 ** i ) * bit
+        i += 1
+
+    return result
+
+
+def get_new_value(x, y, mat):
+    neig = []
+    neig.append(mat[x - 1][y - 1])
+    neig.append(mat[x - 1][y])
+    neig.append(mat[x - 1][y + 1])
+    neig.append(mat[x][y - 1])
+    neig.append(mat[x][y + 1])
+    neig.append(mat[x + 1][y - 1])
+    neig.append(mat[x + 1][y])
+    neig.append(mat[x + 1][y + 1])
+
+    result = []
+    center = mat[x][y]
+    for pixel in neig:
+        if pixel > center:
+            result.append(1)
+        else:
+            result.append(0)
+
+    return binary_to_decimal(result)
+
+
+im1 = cv2.imread('lena.jpg')
+im2 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+matriz = im2.tolist()
+
+census_image = []
+row_temp = []
+for x in range(1, len(matriz) - 1):
+    for y in range(1, len(matriz[0]) - 1):
+        value = get_new_value(x, y, matriz)
+        row_temp.append(value)
+    census_image.append(row_temp)
+    row_temp = []
+im3 = numpy.array(census_image, dtype=numpy.uint8)
+
+cv2.imshow('Orgiginal', im1)
+cv2.imshow('Cambio de color', im3)
+
+cv2.waitKey(0)
